ece7e5f52fe35cc458ae10714f35e15c777d36fc
riva <@>
2026-06-20 19:15:06 +0300

    unit AppLocalizer: example updated

--- examples/AppLocalizer/scripts/copy-i18n-files.bat
+++ examples/AppLocalizer/scripts/copy-i18n-files.bat
@@ -1,36 +1,38 @@
 @chcp 1251
-echo off
 
 
 set PO_UTILITY=scripts\tools\poFileUtility.EXE
 
 echo.
-echo Настройки проекта
+echo Project settings
 echo.
 
 set PROJNAME=AppLocalizer
 set BUILD=Release
 set LANGDIR=bin\lang
+set LANGORIG=en
 
 
 cd ..
 
 echo.
-echo Удаление файлов перевода win32
+echo Removing win64 localization files
+echo Because win32 localization files is used as a base
 echo.
-del /f /q %LANGDIR%\*win32-*.po?
+del /f /q %LANGDIR%\*win64-*.po?
 
 echo.
-echo Копирование win64 файлов перевода в общие для всех бинарников
+echo Copying win32 localization files to files shared by all binaries
 echo.
-copy %LANGDIR%\%PROJNAME%-win64-%BUILD%.*.po  %LANGDIR%\%PROJNAME%.*.po
+copy %LANGDIR%\%PROJNAME%-win32-%BUILD%.*.po  %LANGDIR%\%PROJNAME%.*.po
 
 echo.
-echo Копирование win64 шаблона локализации перевода в общий шаблон
+echo  Copying win32 localization template into common template
 echo.
-copy %LANGDIR%\%PROJNAME%-win64-%BUILD%.pot   %LANGDIR%\%PROJNAME%.pot
+copy %LANGDIR%\%PROJNAME%-win32-%BUILD%.pot   %LANGDIR%\%PROJNAME%.pot
 
 echo.
-echo Перенос строк в файле перевода для языка оригинала и сохранение в .en.po
+echo Transfer lines in localization file for original language and save to ."your-lang".po
 echo.
-%PO_UTILITY% %LANGDIR%\%PROJNAME%.pot %LANGDIR%\%PROJNAME%.en.po transfer
+
+%PO_UTILITY% %LANGDIR%\%PROJNAME%.pot %LANGDIR%\%PROJNAME%.%LANGORIG%.po transfer
